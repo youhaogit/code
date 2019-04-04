47b17f41977af05cc7f700192c156e34828d753c
youhao <hao@aitmed.com>
2019-04-03 17:53:47 -0700

    check

---
+++ Leetcode/p636_ExclusiveTimeofFunctions/Solution.java
@@ -0,0 +1,15 @@
+package Leetcode.p636_ExclusiveTimeofFunctions;
+
+import java.util.List;
+
+public class Solution {
+
+    public int[] exclusiveTime(int n, List<String> logs) {
+
+    }
+
+
+    public static void main(String[] args) {
+        Solution s = new Solution();
+    }
+}
